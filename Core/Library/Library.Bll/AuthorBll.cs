409f147b71d80321cedefd2db8fa7e2f832f01be
Higor Manoel Lima Correa <higor.dev@gmail.com>
2018-07-15 19:42:44 -0300

    Api do Autor

--- Core/Library/Library.Bll/AuthorBll.cs
+++ Core/Library/Library.Bll/AuthorBll.cs
@@ -1,5 +1,11 @@
-﻿using Library.Bll.Interfaces;
+﻿using AutoMapper;
+using Library.Bll.Exceptions;
+using Library.Bll.Interfaces;
+using Library.Domain.DTO.Author;
+using Library.Domain.Entities;
 using Library.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
 
 namespace Library.Bll
 {
@@ -10,5 +16,44 @@ namespace Library.Bll
         public AuthorBll(IAuthorRepository repository)
         { _repository = repository; }
 
+        public AuthorResponseDTO Get(Guid id)
+        {
+            var entity = _repository.Get(id) ?? throw new EntityNotFoundException();
+
+            var response = Mapper.Map<AuthorResponseDTO>(entity);
+
+            return response;
+        }
+
+        public IList<AuthorResponseDTO> GetAll()
+        {
+            return Mapper.Map<List<AuthorResponseDTO>>(_repository.GetAll());
+        }
+
+        public Guid Insert(AuthorRequestDTO request)
+        {
+            var entity = Mapper.Map<AuthorEntity>(request);
+
+            _repository.Insert(entity);
+
+            return entity.Id;
+        }
+
+        public void Update(Guid id, AuthorRequestDTO request)
+        {
+            var entity = _repository.Get(id) ?? throw new EntityNotFoundException();
+
+            entity = Mapper.Map<AuthorEntity>(request);
+            entity.Id = id;
+
+            _repository.Update(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            var entity = _repository.Get(id) ?? throw new EntityNotFoundException();
+
+            _repository.Delete(entity);
+        }
     }
 }
--- Core/Library/Library.Bll/Interfaces/IAuthorBll.cs
+++ Core/Library/Library.Bll/Interfaces/IAuthorBll.cs
@@ -1,5 +1,15 @@
-﻿namespace Library.Bll.Interfaces
+﻿using Library.Domain.DTO.Author;
+using System;
+using System.Collections.Generic;
+
+namespace Library.Bll.Interfaces
 {
     public interface IAuthorBll
-    { }
+    {
+        AuthorResponseDTO Get(Guid id);
+        IList<AuthorResponseDTO> GetAll();
+        Guid Insert(AuthorRequestDTO request);
+        void Update(Guid id, AuthorRequestDTO request);
+        void Delete(Guid id);
+    }
 }
