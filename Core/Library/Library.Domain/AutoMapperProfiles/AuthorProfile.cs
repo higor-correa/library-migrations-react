16864c9d4b3c0d90332450da1c1ac4baca81d2ec
Higor Manoel Lima Correa <higor.dev@gmail.com>
2018-07-31 22:28:53 -0300

    Ajuste do profiler para nao mapear automaticamente os N-N

--- Core/Library/Library.Domain/AutoMapperProfiles/AuthorProfile.cs
+++ Core/Library/Library.Domain/AutoMapperProfiles/AuthorProfile.cs
@@ -1,9 +1,6 @@
 ﻿using AutoMapper;
 using Library.Domain.DTO.Author;
-using Library.Domain.DTO.Book;
 using Library.Domain.Entities;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Library.Domain.AutoMapperProfiles
 {
@@ -12,8 +9,7 @@ namespace Library.Domain.AutoMapperProfiles
         public AuthorProfile()
         {
             CreateMap<AuthorRequestDTO, AuthorEntity>().ForMember(x => x.Id, opt => opt.Ignore());
-            CreateMap<AuthorEntity, AuthorResponseDTO>().ForMember(x => x.Books,
-                opt => opt.MapFrom(x => Mapper.Map<List<BookResponseDTO>>(x.BooksAuthor.Select(b => b.Book))));
+            CreateMap<AuthorEntity, AuthorResponseDTO>().ForMember(x => x.Books, opt => opt.Ignore());
         }
     }
 }
