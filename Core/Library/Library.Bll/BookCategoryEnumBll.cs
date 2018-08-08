5e47f1f234591bece3b3aed9d3f5587f029b2d55
Higor Manoel Lima Correa <higor.dev@gmail.com>
2018-08-07 23:29:20 -0300

    Bll para listar e obter DTOs dos tipos de categoria do livro

---
+++ Core/Library/Library.Bll/BookCategoryEnumBll.cs
@@ -0,0 +1,39 @@
+﻿using Library.Bll.Extensions;
+using Library.Bll.Interfaces;
+using Library.Domain.DTO.BookCategory;
+using Library.Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Library.Bll
+{
+    public class BookCategoryEnumBll : IBookCategoryEnumBll
+    {
+        public BookCategoryResponseDTO Get(BookCategoryEnum bookCategory)
+        {
+            return new BookCategoryResponseDTO
+            {
+                Category = bookCategory,
+                CategoryDescription = bookCategory.GetEnumDescription()
+            };
+        }
+
+        public IList<BookCategoryResponseDTO> GetAll()
+        {
+            var response = new List<BookCategoryResponseDTO>();
+            var enumValues = Enum.GetValues(typeof(BookCategoryEnum));
+
+            foreach (var value in enumValues)
+            {
+                var enumValue = (BookCategoryEnum)value;
+                response.Add(new BookCategoryResponseDTO
+                {
+                    Category = enumValue,
+                    CategoryDescription = enumValue.GetEnumDescription()
+                });
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Core/Library/Library.Bll/Interfaces/IBookCategoryEnumBll.cs
@@ -0,0 +1,12 @@
+﻿using Library.Domain.DTO.BookCategory;
+using Library.Domain.Enums;
+using System.Collections.Generic;
+
+namespace Library.Bll.Interfaces
+{
+    public interface IBookCategoryEnumBll
+    {
+        BookCategoryResponseDTO Get(BookCategoryEnum bookCategory);
+        IList<BookCategoryResponseDTO> GetAll();
+    }
+}
