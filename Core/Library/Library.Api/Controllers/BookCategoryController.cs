552f19904557bb03b9372f30de0861822104ca7a
Higor Manoel Lima Correa <higor.dev@gmail.com>
2018-08-07 23:33:19 -0300

    Exposicao do endpoint rest para listar os tipos de categoria do livro

---
+++ Core/Library/Library.Api/Controllers/BookCategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Library.Bll.Interfaces;
+using Library.Domain.DTO.BookCategory;
+using Library.Domain.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers
+{
+    [Route("api/book-category")]
+    [ApiController]
+    public class BookCategoryController : ControllerBase
+    {
+        private readonly IBookCategoryEnumBll _bookCategoryBll;
+
+        public BookCategoryController(IBookCategoryEnumBll BookCategoryBll)
+        {
+            _bookCategoryBll = BookCategoryBll;
+        }
+
+        [HttpGet("values/{id}")]
+        public IActionResult Get([FromRoute]BookCategoryEnum enumValue)
+        {
+            BookCategoryResponseDTO response = _bookCategoryBll.Get(enumValue);
+
+            return Ok(response);
+        }
+
+        [HttpGet("values")]
+        public IActionResult GetAll()
+        {
+            var response = _bookCategoryBll.GetAll();
+            return Ok(response);
+        }
+    }
+}
