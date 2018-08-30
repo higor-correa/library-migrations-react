ae8bc7315ea0219d9aac2d4853a8da36c517b8f7
Higor Manoel Lima Correa <higor.dev@gmail.com>
2018-08-29 22:09:40 -0300

    Adicao do usuario

---
+++ Core/Library/Library.Domain/Entities/UserEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Domain.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Library/Library.Repository/Context/Mapping/UserMapping.cs
@@ -0,0 +1,28 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Repository.Context.Mapping
+{
+    public class UserMapping : BaseMapping<UserEntity>
+    {
+        public UserMapping(ModelBuilder modelBuilder) : base(modelBuilder)
+        { }
+
+        public override void Map()
+        {
+            _builder.Property(x => x.Login)
+               .IsRequired()
+               .HasMaxLength(10);
+
+            _builder.Property(x => x.Password)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            _builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(60);
+
+            _builder.HasAlternateKey(x => x.Login);
+        }
+    }
+}
