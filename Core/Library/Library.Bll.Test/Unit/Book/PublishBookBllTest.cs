d41578f895552389c2bf67fb88692b6cb89e0b1d
Higor Manoel Lima Correa <higor.dev@gmail.com>
2018-09-15 15:46:32 -0300

    Testes para a regra de negocio para publicacao do livro

--- Core/Library/Library.Bll.Test/Unit/Book/PublishBookBllTest.cs
+++ Core/Library/Library.Bll.Test/Unit/Book/PublishBookBllTest.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentAssertions;
+using Library.Bll.Book;
+using Library.Bll.Book.Validators.Interface;
+using Library.Bll.Test.Builder;
+using Library.Repository.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Library.Bll.Test.Unit.Book
 {
-    class PublishBookBllTest
+    public class PublishBookBllTest
     {
+        private IBookRepository _bookRepository;
+        private IBookPublishValidator _publishBookValidator;
+        private PublishBookBll _publishBookll;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _publishBookValidator = Substitute.For<IBookPublishValidator>();
+            _bookRepository = Substitute.For<IBookRepository>();
+            _publishBookll = new PublishBookBll(_publishBookValidator, _bookRepository);
+        }
+
+        [TestCase]
+        public void Should_Publish_Book()
+        {
+            var publisher = new PublisherBuilder().Build();
+            var book = new BookBuilder().Build();
+
+            _publishBookll.Publish(book, publisher);
+
+            _publishBookValidator.Received(1).ValidatePublish(Arg.Is(book), Arg.Is(publisher));
+
+            book.Publishier.Should().BeEquivalentTo(publisher);
+        }
     }
 }
